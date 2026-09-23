c60f868c44c5e109ba0e5735dd22aaa21ac3d867
Martin <martin@sibbersen.de>
2026-09-23 21:37:01 +0200

    try to fix blue bar at bottom

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service Worker: lädt die App komplett aus dem Cache, damit sie offline läuft.
    Nach Änderungen an den Dateien die Versionsnummer erhöhen. */
-var CACHE = 'routine-app-v5';
+var CACHE = 'routine-app-v6';
 var FILES = [
   './',
   './index.html',
